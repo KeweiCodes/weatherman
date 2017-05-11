0e7608df9773aa8a129b31718b7c70f02f7af4ef
Kewei Jiang <kewei.jiang@cru.org>
2017-05-11 16:10:01 +1200

    Styling

--- app/javascript/blog/components/City.jsx
+++ app/javascript/blog/components/City.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
+import Please from 'pleasejs';
+
+let [c1, c2] = Please.make_scheme(
+  Please.make_color({
+    format: 'hsv',
+    saturdation: '1.0'
+  })[0],
+  {
+    scheme_type: 'complementary'
+  }
+);
+
+let cityStyle = {
+  color: c2,
+  textAlign: 'center'
+}
+
+let rowStyle = {
+  backgroundColor: c1,
+  fontFamily: "'Fredoka One', cursive",
+  textShadow: '1px 1px white',
+  fontSize: '28px'
+}
+
+let inputStyle = {
+  background: 'transparent',
+  height: '42px',
+  border: '2px solid darkslategray',
+  fontFamily: "'Frank Ruhl Libre', sans-serif",
+  color: 'white',
+  textShadow: '1px 1px black'
+}
 
 class City extends React.Component{
   render(){
     let { city } = this.props;
     return (
-      <div>
-        <h2>{city.name}, {city.country}</h2>
+      <div style={rowStyle} className="row rowStyle">
+        <div className="col-md-2">
+          Enter City:
+        </div>
+        <div className="col-md-7">
+          <input type='text' style={inputStyle} className="form-control"/>
+        </div>
+        <div className="col-md-3" style={cityStyle}>{city.name}, {city.country}</div>
       </div>
     )
   }
--- app/javascript/blog/components/Main.jsx
+++ app/javascript/blog/components/Main.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import Unit from './Unit';
 import City from './City';
 
+let style = {
+  fontFamily: "'Frank Ruhl Libre', sans-serif",
+  textAlign: 'center'
+}
+
 class Main extends React.Component{
   render(){
     let { data, city } = this.props;
     return (
       <div>
-        <h2>THis is Main</h2>
+        <h2 style={style}>Weather App</h2>
         <City city={city}/>
         { data.map((dataUnit, i) => <Unit key={i} data={dataUnit} />) }
       </div>
