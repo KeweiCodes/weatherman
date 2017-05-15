709f6a000b2864a50b61b295ed1ec4ee379b8144
Kewei Jiang <kewei.jiang@cru.org>
2017-05-15 15:51:30 +1200

    Authentication stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery prepend: true, with: :exception
+  before_action :authenticate_user!
 
   def index
     render file: "../views/layouts/application"
