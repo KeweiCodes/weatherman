80a419ff0aa28e1768598f0437ded3191d72c335
Kewei Jiang <kewei.jiang@cru.org>
2017-05-17 12:26:15 +1200

    OAuth 2 working!

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'devise', git: 'https://github.com/plataformatec/devise.git', branch: 'master'
 
+gem 'doorkeeper'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/javascript/blog/index.jsx
+++ app/javascript/blog/index.jsx
@@ -7,6 +7,21 @@ import App from './components/App';
 
 import store, { history } from './store';
 
+import JSO from './common/JSO';
+
+let jso = new JSO({
+  providerID: 'weather',
+  client_id: 'f046d279e2018aed4b0686c677ad8034667c4116f38cde220d4b784cb31c1fb2',
+  redirect_uri: 'http://localhost:3000',
+  authorization: 'http://localhost:3000/oauth/authorize',
+  debug: false
+})
+
+jso.callback(null, null, 'weather');
+
+jso.getToken(function(token) {
+  window._token = token.access_token;
+});
 
 const router = (
   <Provider store={store}>
